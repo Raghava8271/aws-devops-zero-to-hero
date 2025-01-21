b2ad9ce90d4d0d71055a265b818e6468f303c548
Raghava8271 <125386718+Raghava8271@users.noreply.github.com>
2025-01-21 20:10:42 +0530

    for codedeploy to ec2

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-# Pull the Docker image from Docker Hub
-echo
+Pull the Docker image from Docker Hub
+docker pull raghav8271/simple-python-flask-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 raghav8271/simple-python-flask-app
